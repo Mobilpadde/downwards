22b295e3f8d9791b4ce8263ebd23106f29e1799f
Mads Cordes <Mobilpadde@gmail.com>
2022-06-29 11:17:55 +0200

    Use proper time for clock

--- js/utils/clock.js
+++ js/utils/clock.js
@@ -1,18 +1,9 @@
-const yearStart = ~~(Math.random() * 5000);
-
-let time = 0;
-
-const c = setInterval(() => time++, 5);
-const stop = () => clearInterval(c);
-
 export default () => {
-  let s = time % 60;
-  let m = ~~(time / 60) % 60;
-  let h = ~~(time / 60 / 60) % 24;
+  const now = new Date();
 
-  let day = ~~(time / 60 / 60 / 24) + 1;
-  let month = ~~(time / 60 / 60 / 24 / 30) + 1;
-  let year = ~~(time / 60 / 60 / 24 / 30 / 365) + yearStart + 1;
+  let s = now.getSeconds();
+  let m = now.getMinutes();
+  let h = now.getHours();
 
   if (s < 10) {
     s = `0${s}`;
@@ -26,14 +17,5 @@ export default () => {
     h = `0${h}`;
   }
 
-  if (day < 10) {
-    day = `0${day}`;
-  }
-
-  if (month < 10) {
-    month = `0${month}`;
-  }
-
-  return `${year}/${month}/${day} ${h}:${m}:${s}`;
+  return `${h}:${m}:${s}`;
 };
-export { stop };
